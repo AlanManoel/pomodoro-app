46637d56a236d5f845cc6629ce0cff2935e0a64a
Alan Manoel <alanmanoel517@gmail.com>
2026-07-13 10:24:57 -0300

    feat: add base folders structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+
+
+export const Home = () => {
+    return (
+        <View>
+            <Text>Home page</Text>
+        </View>
+
+    )
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+
+
+
+export const Settings = () => {
+    return (
+        <View>
+            <Text>Settintgs page</Text>
+        </View>
+
+    )
+}

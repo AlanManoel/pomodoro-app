ff3264d0dce1606039eb3542ea977988f7af87d9
Alan Manoel <alanmanoel517@gmail.com>
2026-07-14 10:18:45 -0300

    feat: config safe area view

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 import { AppRoutes } from './AppRoutes';
+import { Theme } from './shared/Themes/Theme';
+import { StatusBar } from 'expo-status-bar';
 
 
 SplashScreen.preventAutoHideAsync();
@@ -27,5 +30,10 @@ export function App() {
     return null;
   }
 
-  return <AppRoutes />
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: Theme.colors.background }}>
+      <StatusBar style='light' />
+      <AppRoutes />
+    </SafeAreaView>
+  );
 }

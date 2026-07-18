3c7ddee133aaa61bf600c983eb26aa7354476cd0
Alan Manoel <alanmanoel517@gmail.com>
2026-07-18 15:09:24 -0300

    feat: modifeld

--- src/shared/components/PomodoroProgress/index.tsx
+++ src/shared/components/PomodoroProgress/index.tsx
@@ -4,7 +4,7 @@ import { styles } from "./styles";
 
 type Props = {
   step: 1 | 2 | 3 | 4;
-  index: 1 | 2 | 3 | 4;
+  index: 1 | 2 | 3 | 4 | 5;
 };
 
 export function PomodoroProgress({ step, index }: Props) {
